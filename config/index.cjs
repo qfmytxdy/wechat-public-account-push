96bffa6db4a992afc9a81dda7862a9ec64f96b05
qfmytxdy <106159247+qfmytxdy@users.noreply.github.com>
2023-06-30 17:13:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ouqxS6aLJaEMC5G6R9lRbsJAA5_8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'bkdOUriGobVIcgEqcRlJPZfPKkN5ZUyCFqHGZAFMQfk',
+      useTemplateId: 'pJycSUFbeiJw42_F5ed0YygAw0lPLND43NFETsuLAkE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '3-14',
       festivals: [
